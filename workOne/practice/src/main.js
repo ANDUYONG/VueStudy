e19675d9bbec6d6a266d5816725e1e71b1ff5c9b
ady <du2sam@naver.com>
2024-02-07 11:28:19 +0900

    2024/02/07 안두용 업데이트 완료

--- workOne/practice/src/main.js
+++ workOne/practice/src/main.js
@@ -1,18 +1,10 @@
 
 import { createApp } from 'vue';
-//app 실행
- import App from './App.vue';
+import App from './App.vue';
 import router from '@/router';
-// import { routes } from './routes';
 
-//parent 실행
-//import App from './components/parent.vue';
-
-
-// import App from './family/parent.vue';
 const app = createApp(App);
 
 app.use(router);
 app.mount('#app');
-// app.use(routes)
 
